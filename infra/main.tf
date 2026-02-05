542c4d2868444bbce07a2efe2c536ad935dcd1ec
mazin1hn <mazinwork@outlook.com>
2026-02-05 02:46:35 +0000

    update:root  main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -7,10 +7,8 @@ module "vpc" {
   private_subnet_b_cidr_block = var.private_subnet_b_cidr_block
   public_subnet_a_cidr_block  = var.public_subnet_a_cidr_block
   public_subnet_b_cidr_block  = var.public_subnet_b_cidr_block
-  private_subnet_a_az = var.private_subnet_a_az
-  private_subnet_b_az = var.private_subnet_b_az
-  public_subnet_a_az = var.public_subnet_a_az
-  public_subnet_b_az = var.public_subnet_b_az
+  az_a = var.az_a
+  az_b = var.az_b 
 
-}
+}s
   
