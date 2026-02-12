8edab5834183ee843f7d029fa2d1b30ba07a7a50
mazin1hn <mazinwork@outlook.com>
2026-02-12 04:32:09 +0000

    add: iniial root main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_id = module.vpc.vpc_id
+  
   vpc_cidr_block = var.vpc_cidr_block
   private_subnet_a_cidr_block = var.private_subnet_a_cidr_block
   private_subnet_b_cidr_block = var.private_subnet_b_cidr_block
@@ -10,5 +10,20 @@ module "vpc" {
   az_a = var.az_a
   az_b = var.az_b 
 
-}s
+}
+
+module "iam" {
+    source = "./modules/iam"
+}
+
+
+module "eks" {
+    source = "./modules/eks"
+  
+private_subnet_ids = module.vpc.private_subnet_ids
+vpc_id = module.iam.vpc_id
+
+
+
+}
   
