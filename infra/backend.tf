e20fd5a506e11396fa39a0afcc880ca8e61506de
mazin1hn <mazinwork@outlook.com>
2026-02-20 14:37:57 +0000

    fix: s3 bucket name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "mazin-s3-ecs-bucket"
+    bucket       = "mazin-eks-s3-bucket"
     key          = "infra/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true
