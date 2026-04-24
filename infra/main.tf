f1d6ba968d1a7f4447068698eafa252a7fafa5c3
mazin1hn <mazinwork@outlook.com>
2026-04-24 15:43:04 +0100

    update: ebs csi driver version

--- infra/main.tf
+++ infra/main.tf
@@ -34,7 +34,7 @@ module "oidc" {
 resource "aws_eks_addon" "ebs_csi_driver" {
     cluster_name = module.eks.cluster_name
     addon_name = "ebs_csi_driver"
-    addon_version = "1.35.0"
+    addon_version = "v1.59.0-eksbuild.1"
     depends_on = [module.eks] 
     resolve_conflicts_on_create = "OVERWRITE"
     resolve_conflicts_on_update = "OVERWRITE"
