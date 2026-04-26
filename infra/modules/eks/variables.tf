42415c3bac01f1acde2e8626849dfee82a1c6fd2
mazin1hn <mazinwork@outlook.com>
2026-04-26 19:38:47 +0100

    test:gitops

--- infra/modules/eks/variables.tf
+++ infra/modules/eks/variables.tf
@@ -82,13 +82,13 @@ variable "eks_node_group_name" {
 
 variable "eks_node_group_desired_size" {
     type = number 
-    default = 2
+    default = 3
   
 }
 
 variable "eks_node_group_max_size" {
     type = number 
-    default = 3
+    default = 6
   
 }
 
