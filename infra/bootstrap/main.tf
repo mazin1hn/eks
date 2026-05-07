8ea87e3bf7e1547f70effc5350bae3ca5f5ab2a9
mazin1hn <mazinwork@outlook.com>
2026-05-07 16:36:45 +0100

    add: s3 block public access

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -17,3 +17,12 @@ resource "aws_s3_bucket" "eks" {
     
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "eks" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
